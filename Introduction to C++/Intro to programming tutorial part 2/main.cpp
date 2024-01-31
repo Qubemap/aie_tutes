dfb049722e119659a0adc06dc6e4ae6723913cec
Qubemap <87596622+Qubemap@users.noreply.github.com>
2024-01-31 06:20:31 +0000

    second

---
+++ Introduction to C++/Intro to programming tutorial part 2/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+
+/* 2 */
+/*
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    std::cin >> a >> b;
+
+    if (a > b)
+    {
+        std::cout << a << std::endl;
+    }
+    else
+    {
+        std::cout << b << std::endl;
+    }
+
+    return 0;
+}
+*/
+
+//3
+/*
+int main()
+{
+    int a, b, c, d, e;
+    int total;
+
+    total = a = b = c = d = e = 0;
+
+    std::cin >> a >> b >> c >> d >> e;
+
+    total = a + b + c + d + e;
+
+    if (total > 0)
+    {
+        std::cout << "positive" << std::endl;
+    }
+    else if (total == 0)
+    {
+        std::cout << "zero" << std::endl;
+    }
+    else
+    {
+        std::cout << "negative" << std::endl;
+    }
+    return 0;
+    
+}
+*/
+
+//4
+/*
+int main()
+{
+	int choice = 2;
+
+    switch(choice)
+    {
+		case 1:
+			std::cout << "one" << std::endl;
+			break;
+		case 2:
+		case 3:
+			std::cout << "two or three" << std::endl;
+			break;
+		case 4:
+			std::cout << "four" << std::endl;
+			break;
+		default:
+			std::cout << "Invalid" << std::endl;
+	}
+}
+*/
+
+int main()
+{
+	int x = 0;
+	y = (x == 0) ? 0 : 10 / x;
+}
